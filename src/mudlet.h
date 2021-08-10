acfcadb7067cd7d789e29fcda5c6503056182ebe
Stephen Lyons <slysven@virginmedia.com>
2021-08-10 23:25:59 +0100

    Revise: make the list of default games a public compile time constant
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- src/mudlet.h
+++ src/mudlet.h
@@ -258,7 +258,6 @@ public:
     void stopSounds();
     void playSound(const QString &s, int);
     QStringList getAvailableFonts();
-    const QStringList& getDefaultGames() const { return mDefaultGames; }
     std::pair<bool, QString> setProfileIcon(const QString& profile, const QString& newIconPath);
     std::pair<bool, QString> resetProfileIcon(const QString& profile);
 #if defined(Q_OS_WIN32)
@@ -431,6 +430,31 @@ public:
     // Options dialog when there's no active host
     QPointer<dlgProfilePreferences> mpDlgProfilePreferences;
 
+    inline static const QStringList scmDefaultGames {QStringLiteral("3Kingdoms"),
+                                                     QStringLiteral("3Scapes"),
+                                                     QStringLiteral("Aardwolf"),
+                                                     QStringLiteral("Achaea"),
+                                                     QStringLiteral("Aetolia"),
+                                                     QStringLiteral("Avalon.de"),
+                                                     QStringLiteral("BatMUD"),
+                                                     QStringLiteral("Clessidra"),
+                                                     QStringLiteral("Fierymud"),
+                                                     QStringLiteral("Imperian"),
+                                                     QStringLiteral("Luminari"),
+                                                     QStringLiteral("Lusternia"),
+                                                     QStringLiteral("Materia Magica"),
+                                                     QStringLiteral("Midnight Sun 2"),
+                                                     QStringLiteral("Realms of Despair"),
+                                                     QStringLiteral("Reinos de Leyenda"),
+                                                     QStringLiteral("StickMUD"),
+                                                     QStringLiteral("WoTMUD"),
+                                                     QStringLiteral("ZombieMUD"),
+                                                     QStringLiteral("Carrion Fields"),
+                                                     QStringLiteral("Cleft of Dimensions"),
+                                                     QStringLiteral("CoreMUD"),
+                                                     QStringLiteral("God Wars II"),
+                                                     QStringLiteral("Slothmud"),
+                                                     QStringLiteral("Legends of the Jedi")};
 
 public slots:
     void processEventLoopHack_timerRun();
@@ -652,32 +676,6 @@ private:
 
     // Whether multi-view is in effect:
     bool mMultiView;
-
-    const QStringList mDefaultGames = {"3Kingdoms",
-                                       "3Scapes",
-                                       "Aardwolf",
-                                       "Achaea",
-                                       "Aetolia",
-                                       "Avalon.de",
-                                       "BatMUD",
-                                       "Clessidra",
-                                       "Fierymud",
-                                       "Imperian",
-                                       "Luminari",
-                                       "Lusternia",
-                                       "Materia Magica",
-                                       "Midnight Sun 2",
-                                       "Realms of Despair",
-                                       "Reinos de Leyenda",
-                                       "StickMUD",
-                                       "WoTMUD",
-                                       "ZombieMUD",
-                                       "Carrion Fields",
-                                       "Cleft of Dimensions",
-                                       "CoreMUD",
-                                       "God Wars II",
-                                       "Slothmud",
-                                       "Legends of the Jedi"};
 };
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(mudlet::controlsVisibility)
